77025bd5aebd4808a52519c29da7835645fd256e
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-17 22:32:08 +0200

    Better GUI

--- src/Start.java
+++ src/Start.java
@@ -1,21 +1,27 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Start extends Application {
+    // Change board size here.
+    private final int BOARD_X = 50;
+    private final int BOARD_Y = 50;
+
     Game game = new Game();
     GridPane gridPane = new GridPane();
     Timeline timer;
+    Label iterationLabel;
+    int iteration;
     boolean timerStarted;
 
     @Override
@@ -23,13 +29,16 @@ public class Start extends Application {
         gridPane.setGridLinesVisible(true);
         gridPane.setVgap(2);
         gridPane.setHgap(2);
-        game.populateBoard(100, 100);
+        gridPane.setStyle("-fx-border-color: black");
+        iterationLabel = new Label();
+        game.populateBoard(BOARD_X, BOARD_Y);
         draw();
         timer = new Timeline(new KeyFrame(Duration.millis(250), actionEvent -> {
             game.update();
             draw();
         }));
         timer.setCycleCount(Timeline.INDEFINITE);
+
         Button button = new Button("Start/stop");
         button.setOnAction(actionEvent -> {
             if (timerStarted) {
@@ -40,23 +49,29 @@ public class Start extends Application {
                 timerStarted = true;
             }
         });
-        VBox vBox = new VBox(gridPane, button);
+        HBox hBox = new HBox(iterationLabel, button);
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        VBox vBox = new VBox(gridPane, hBox);
+        vBox.setStyle("-fx-padding: 16");
         vBox.setSpacing(48);
-        vBox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(vBox);
         stage.setScene(scene);
+        stage.setTitle("Game of Life");
         stage.show();
     }
 
     public void draw() {
+        iteration++;
+        iterationLabel.setText("Iteration: " + iteration);
         gridPane.getChildren().clear();
         for (int i = 0; i < game.getBoard().length; i++) {
             for (int j = 0; j < game.getBoard()[i].length; j++) {
                 Pane pane = new Pane();
-                pane.setPrefHeight(5);
-                pane.setPrefWidth(5);
+                pane.setPrefWidth(6);
+                pane.setPrefHeight(6);
                 if (game.getBoard()[i][j].isAlive()) {
-                    pane.setStyle("-fx-background-color: #000000");
+                    pane.setStyle("-fx-background-color: black");
                 }
                 else {
                     pane.setStyle("-fx-background-color: white");
