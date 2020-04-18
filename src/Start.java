68aa489b051ecd7c40493bcd63719ef74b8d0219
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-18 09:23:22 +0200

    Added mouse events changed draw process and added more to log.

--- src/Start.java
+++ src/Start.java
@@ -24,6 +24,7 @@ public class Start extends Application {
 
     Game game = new Game();
     GridPane gridPane = new GridPane();
+    Rectangle[][] rectangles;
     Timeline timer;
     Label iterationLabel;
     int iteration;
@@ -36,8 +37,10 @@ public class Start extends Application {
         gridPane.setVgap(4);
         iterationLabel = new Label();
         game.populateBoard(BOARD_X, BOARD_Y);
+        rectangles = new Rectangle[BOARD_X][BOARD_Y];
+        makeInitialRectangles();
         draw();
-        timer = new Timeline(new KeyFrame(Duration.millis(250), actionEvent -> {
+        timer = new Timeline(new KeyFrame(Duration.millis(100), actionEvent -> {
             game.update();
             draw();
         }));
@@ -45,43 +48,26 @@ public class Start extends Application {
 
         Button button = new Button("Start/stop");
         button.setOnAction(actionEvent -> {
-            String status;
+            String textToLog;
             if (timerStarted) {
                 timer.pause();
                 timerStarted = false;
-                status = "stopped at iteration " + iteration;
+                textToLog = "Game of life stopped at iteration " + iteration + "\n\n";
             } else {
                 timer.play();
                 timerStarted = true;
-                status = "started at iteration " + iteration;
+                textToLog = "Game of life started at iteration " + iteration + "\n\n";
             }
-            Thread thread = new Thread(() -> {
-                File file = new File("log/logFile.txt");
-
-                try {
-                    if (file.exists()) {
-                        FileWriter writer = new FileWriter(file, true);
-                        writer.append("Game of Life ").append(status).append("\n\n");
-                        writer.close();
-                    }
-                    else {
-                        FileWriter writer = new FileWriter(file);
-                        writer.write("Actions:\n");
-                        writer.write("Game of Life " + status + "\n\n");
-                        writer.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            thread.start();
+            writeToLog(textToLog);
         });
         HBox hBox = new HBox(iterationLabel, button);
         hBox.setSpacing(10);
         hBox.setAlignment(Pos.CENTER);
-        VBox vBox = new VBox(gridPane, hBox);
+        Label label = new Label("Click on dead cells to make them alive when the game is stopped.");
+        VBox vBox = new VBox(gridPane, label, hBox);
         vBox.setStyle("-fx-padding: 16");
-        vBox.setSpacing(48);
+        vBox.setSpacing(16);
+        vBox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(vBox);
         stage.setScene(scene);
         stage.setResizable(false);
@@ -89,23 +75,74 @@ public class Start extends Application {
         stage.show();
     }
 
-    public void draw() {
-        iteration++;
-        iterationLabel.setText("Iteration: " + iteration);
-        gridPane.getChildren().clear();
+    private void makeInitialRectangles() {
         for (int i = 0; i < game.getBoard().length; i++) {
             for (int j = 0; j < game.getBoard()[i].length; j++) {
                 Rectangle rectangle = new Rectangle();
+                rectangles[i][j] = rectangle;
+                int finalJ = j;
+                int finalI = i;
+                rectangle.setOnMouseEntered(mouseEvent -> {
+                    if (!game.getBoard()[finalI][finalJ].isAlive() && !timerStarted) {
+                        rectangle.setFill(Color.ORANGE);
+                    }
+                });
+                rectangle.setOnMouseExited(mouseEvent -> {
+                    if (!game.getBoard()[finalI][finalJ].isAlive() && !timerStarted) {
+                        rectangle.setFill(Color.WHITE);
+                    }
+                });
+                rectangle.setOnMouseClicked(mouseEvent -> {
+                    if (!game.getBoard()[finalI][finalJ].isAlive() && !timerStarted) {
+                        game.getBoard()[finalI][finalJ].setAlive(true);
+                        rectangle.setFill(Color.BLACK);
+                        writeToLog("Cell revived at " + finalI + ", " + finalJ + "\n\n");
+                    }
+
+                });
                 rectangle.setHeight(8);
                 rectangle.setWidth(8);
+                gridPane.add(rectangle, i, j);
+            }
+        }
+    }
+
+    private void draw() {
+        iteration++;
+        iterationLabel.setText("Iteration: " + iteration);
+        for (int i = 0; i < game.getBoard().length; i++) {
+            for (int j = 0; j < game.getBoard()[i].length; j++) {
+                Rectangle rectangle = rectangles[i][j];
                 if (game.getBoard()[i][j].isAlive()) {
                     rectangle.setFill(Color.BLACK);
                 }
                 else {
                     rectangle.setFill(Color.WHITE);
                 }
-                gridPane.add(rectangle, i, j);
             }
         }
+    }
+
+    private void writeToLog(String text) {
+        Thread thread = new Thread(() -> {
+            File file = new File("log/logFile.txt");
+
+            try {
+                if (file.exists()) {
+                    FileWriter writer = new FileWriter(file, true);
+                    writer.append(text);
+                    writer.close();
+                }
+                else {
+                    FileWriter writer = new FileWriter(file);
+                    writer.write("Actions:\n");
+                    writer.write(text);
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
     }
 }
